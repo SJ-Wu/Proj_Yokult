1f44b705b33c4544798d5a0ea05ba8c995e5ab37
24_Peggy <peggy8179@gmail.com>
2022-08-01 13:58:22 +0800

    update-productlist

--- src/main/java/web/product/servlet/ProductServlet.java
+++ src/main/java/web/product/servlet/ProductServlet.java
@@ -1,8 +1,10 @@
 package web.product.servlet;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Set;
 
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,26 +23,26 @@ import web.product.service.impl.ProductServiceImpl;
 import web.product.vo.Product;
 
 @WebServlet("/api/0.01/product/*")
-public class ProductServlet extends HttpServlet{
+public class ProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
 //	private String pathInfo;
 //	private String[] infos;
 	private ProductService service;
-	
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("UTF-8");
 		setHeaders(resp);
 		JsonObject respObject = new JsonObject();
-		
+
 		String category = req.getParameter("category");
-		
+		String productName = req.getParameter("productName");
+
 		System.out.println(category);
 		try {
 			service = new ProductServiceImpl();
-			Set<Product> products = service.getAll(category);
+			List<Product> products = service.getAll(category, productName);
 			if (products != null) {
 				respObject.addProperty("msg", "success");
 				respObject.add("products", gson.toJsonTree(products));
@@ -51,11 +53,25 @@ public class ProductServlet extends HttpServlet{
 		resp.getWriter().append(gson.toJson(respObject));
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		Product product = gson.fromJson(req.getReader(), Product.class);
+		System.out.println(product.toString());
+
+		try {
+			service = new ProductServiceImpl();
+			service.save(product);
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+
+		resp.getWriter().append(gson.toJson(""));
+	}
 
 	private void setHeaders(HttpServletResponse resp) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
